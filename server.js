6cc71bbd2e47f56bcf2be86d4726bef0031af301
Prajwal Goswami <prajwalgoswami1996@gmail.com>
2019-01-19 00:09:43 +0530

    Added API and DB connections
    removed API call from frontend to backend. Added postgres db links.

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: 'localhost',
+        user: 'postgres',
+        password: 'test',
+        database: 'facerecognition'
+
+    }
+});
+
+db.select('*').from('users').then(data => {
+    console.log
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send(database.users);
+});
+
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)});
+
+app.post('/register', register.handleRegister(db, bcrypt));
+
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)});
+
+app.put('/image', (req, res) => { image.handleImage(req, res, db)});
+
+app.post('/imageUrl', (req, res) => { image.handleApiCall(req, res)});
+
+app.listen(3001, () => {
+    console.log('app workin at port 3001');
+});
