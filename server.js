c18e93813ea931ba7ec03385119e193e944d55af
Prajwal Goswami <prajwalgoswami1996@gmail.com>
2019-01-19 01:09:11 +0530

    Added heroku postgres

--- server.js
+++ server.js
@@ -12,13 +12,21 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: 'localhost',
-        user: 'postgres',
-        password: 'test',
-        database: 'facerecognition'
-
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
     }
 });
+
+// const db = knex({
+//     client: 'pg',
+//     connection: {
+//         host: 'localhost',
+//         user: 'postgres',
+//         password: 'test',
+//         database: 'facerecognition'
+
+//     }
+// });
 
 db.select('*').from('users').then(data => {
     console.log
